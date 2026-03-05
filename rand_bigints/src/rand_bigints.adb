c0e706c56d563d5dcab49ace3d29843e69171cc7
AldanTanneo <aldantanneo@gmail.com>
2026-03-05 14:25:13 +0100

    unnecessary parens

--- rand_bigints/src/rand_bigints.adb
+++ rand_bigints/src/rand_bigints.adb
@@ -1,6 +1,6 @@
 package body Rand_Bigints is
    function Generic_Gen (R : in out Rng) return Uints.Uint
-   is ([for I in 1 .. Uints.N => R.Next]);
+   is [for I in 1 .. Uints.N => R.Next];
 
    function Gen_Classwide is new Generic_Gen (Rand_Core.Generators.Rng'Class);
    function Gen (R : in out Rand_Core.Generators.Rng'Class) return Uints.Uint
